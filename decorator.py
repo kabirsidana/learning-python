8fb6170b16268682edfed32f83890ff7db59285e
Kabir Sidana <96888292+kabirsidana@users.noreply.github.com>
2022-10-28 16:02:36 +0530

    Add files via upload

---
+++ decorator.py
@@ -0,0 +1,19 @@
+# def arman(func1):
+#     print("hello")
+#     func1()
+#     print("Byee")
+def arman(myname):
+    print("Hello ")
+    myname()
+    print("Bye")
+
+@arman
+def kabir():
+    print("My Name is kabir")
+def bio():
+    print("My name is Arman ")
+# arman(bio)
+# bio=arman(bio)
+#bio
+
+
---
+++ reverse.py
@@ -0,0 +1,5 @@
+num=int(input("Enter a Number "))
+while(num>1):
+    a=num%10
+    print(int(a),end="")
+    num=num/10
