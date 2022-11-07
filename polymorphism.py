5d084ae478323ad036882b134679bf492f327f1a
Kabir Sidana <96888292+kabirsidana@users.noreply.github.com>
2022-11-07 19:12:17 +0530

    OOP's

---
+++ polymorphism.py
@@ -0,0 +1,28 @@
+# The word polymorphism means having many forms
+# use of a single type entity (method, operator or object) to represent
+#  different types in different scenarios.
+
+''' +Operator '''
+#addition (int)
+a=10;b=12
+print(a+b)
+
+#concatination (str)
+c="hello"
+d="world"
+print(c+d)
+
+'''len() function'''
+
+#string length
+random_str="my name is kabir "
+print(len(random_str))
+
+#Number of items in list
+random_list=[1,2,45,65,6,76,8,76]
+print(len(random_list))
+
+#Number of keys in dictionary
+random_dict={"name":"kabir","age":"18","blood group":"B+"} 
+print(len(random_dict))
+
