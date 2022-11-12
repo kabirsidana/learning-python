cb17751b8bd9abf59caabf789a0f1f87e442f6bb
Kabir Sidana <96888292+kabirsidana@users.noreply.github.com>
2022-11-12 23:59:07 +0530

    Update primenumber.py

--- primenumber.py
+++ primenumber.py
@@ -11,3 +11,35 @@ else:
     print("it is a Prime Number")
 
 print("end")
+
+
+# Checking Prime Number with class and object
+'''
+class Prime:
+    def __init__(self,num):
+        self.num=num
+    def process(self):
+        temp=self.num
+        a=0
+        for i in range(1,self.num):
+            if((temp%i)==0):
+                print(" Not a Prime Number")
+                break
+        else:
+            print("Prime")    
+
+ob=Prime(12)
+ob.process()
+        '''
+
+
+# Prime Number between 1-100
+'''
+for n in range(5,300+1):
+
+    for i in range(2,n):
+        if(n%i==0):
+            break
+    else:
+        print(n)       
+'''
